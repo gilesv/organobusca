f530ecb9e57a61e0c04b06f987d173855bf9c40a
Vinícius Giles <viniciusgcp@gmail.com>
2017-05-29 11:17:08 -0300

    Novo projeto com individual user authentication

---
+++ Organobusca/Organobusca_/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Organobusca_.MvcApplication" Language="C#" %>
---
+++ Organobusca/Organobusca_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Organobusca_.Startup))]
+namespace Organobusca_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
