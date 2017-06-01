3ad070f819a51445fb084009ab46ccbdacc87955
spike <spike@DESKTOP-QA5J285>
2017-06-01 12:33:26 -0300

    Atualização

--- Organobusca/Organobusca/Controllers/HomeController.cs
+++ Organobusca/Organobusca/Controllers/HomeController.cs
@@ -12,7 +12,25 @@ namespace Organobusca.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var SESSAO = Session["usuario"];
+            if (SESSAO != null)
+            {
+                if (SESSAO is Feirante)
+                    return RedirectToAction("IndexFeirante", "Dashboard");
+                else if (SESSAO is Cliente)
+                    return RedirectToAction("Index", "Dashboard");
+            }
             return View();
+
+            //if(Session["cliente"] != null)
+            //{
+            //    return RedirectToAction("Index", "Dashboard");
+            //}
+            //else if (Session["feirante"] != null)
+            //{
+            //    return RedirectToAction("IndexFeirante", "Dashboard");
+            //}
+            //return View();
         }
     }
 }
